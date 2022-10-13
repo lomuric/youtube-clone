a63ca542d692d8e31eef6cfa14d889d0d40ff31c
kibuny njolge <knjolge@gmail.com>
2022-10-13 16:18:55 +0100

    Complete

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
-const SideBar = ({selectedCategory, setSelectedCategory}) => {
+const SideBar = ({ selectedCategory, setSelectedCategory }) => {
   return (
     <Stack
       direction="row"
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,21 +4,21 @@ import { Stack, Box } from "@mui/material";
 import { ChannelCard, VideoCard } from "./";
 
 const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...'
   return (
     <Stack
-      direction="row"
+      direction={direction || "row"}
       flexWrap="wrap"
       justifyContent="start"
       alignItems="start"
       gap={2}
     >
-      {videos &&
-        videos.map((item, idx) => (
-          <Box key={idx}>
-            {item.id.videoId && <VideoCard video={item} />}
-            {item.id.channelId && <ChannelCard channelDetail={item} />}
-          </Box>
-        ))}
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
     </Stack>
   );
 };
